e826860d5c19ffbebdaa9beffe1d442136c26432
victor <victor_cardoso00@hotmail.com>
2019-08-02 00:42:31 -0300

    tela pedido 90%

--- front/includes/js/order.js
+++ front/includes/js/order.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-	var DOMAIN = "http://localhost/inv_project/public_html";
-
 	$('.date').mask('00/00/0000', {placeholder: "__/__/____"});
-	$('.money').mask('000.000.000.000.000,00', {reverse: true});
-	$('.time').mask('00:00:00');
+	$('.money').mask("#.##0,00", {reverse: true});
 
 	var today = new Date();
 	var dd = String(today.getDate()).padStart(2, '0');
@@ -28,31 +25,27 @@ $(document).ready(function(){
 	};
 
 	$(".removeitem").on('click',function(){
-		console.log('oi');
+		$(this).parent().parent().remove();
 		calculate(0,0);
 	});
 
-	$("#invoice_item").delegate(".pid","change",function(){
+	$("#invoice_item").delegate(".selectpicker","change",function(){
 		var pid = $(this).val();
-		var tr = $(this).parent().parent();
+		var tr = $(this).parent().parent().parent();
 		$(".overlay").show();
-		$.ajax({
-			url : DOMAIN+"/includes/process.php",
-			method : "POST",
-			dataType : "json",
-			data : {getPriceAndQty:1,id:pid},
-			success : function(data){
-				tr.find(".tqty").val(data["product_stock"]);
-				tr.find(".pro_name").val(data["product_name"]);
-				tr.find(".qty").val(1);
-				tr.find(".price").val(data["product_price"]);
-				tr.find(".amt").html( tr.find(".qty").val() * tr.find(".price").val() );
-				calculate(0,0);
-			}
-		})
+		setTimeout(function(){
+			var estoque = Math.floor((Math.random() * 100) + 1);
+			var precounitario = Math.floor((Math.random() * 10000) + 1);
+
+			tr.find(".estoque").val(estoque);
+			tr.find(".precounitario").unmask().val(precounitario).mask("#.##0,00", {reverse: true});
+			tr.find(".preco").unmask().val(precounitario).mask("#.##0,00", {reverse: true});
+			calculate();
+			$(".overlay").hide();
+		}, 500);
 	});
 
-	$("#invoice_item").delegate(".quantidade","keyup",function(){
+	$("#invoice_item").delegate(".quantidade","focusout",function(){
 		var qty = $(this);
 		var tr = $(this).parent().parent();
 		if (isNaN(qty.val())) {
@@ -63,35 +56,19 @@ $(document).ready(function(){
 				alert("Quantidade não disponível no estoque!");
 				qty.val(1);
 			}else{
-				tr.find(".preco").unmask().val(qty.val() * tr.find(".precounitario").cleanVal()).mask('000.000.000.000.000,00', {reverse: true});
-
-				calculate(0,0);
+				tr.find(".preco").unmask().val(qty.val() * tr.find(".precounitario").cleanVal()).mask("#.##0,00", {reverse: true});
+				calculate();
 			}
 		}
 	});
 
-	function calculate(dis,paid){
+	function calculate(){
 		var sub_total = 0;
-		var gst = 0;
-		var net_total = 0;
-		var discount = dis;
-		var paid_amt = paid;
-		var due = 0;
-		$(".amt").each(function(){
-			sub_total = sub_total + ($(this).html() * 1);
+		$(".preco").each(function(){
+			sub_total = sub_total + ($(this).cleanVal() * 1);
 		})
-		gst = 0.18 * sub_total;
-		net_total = gst + sub_total;
-		net_total = net_total - discount;
-		due = net_total - paid_amt;
-		$("#gst").val(gst);
-		$("#sub_total").val(sub_total);
-		
-		$("#discount").val(discount);
-		$("#net_total").val(net_total);
-		//$("#paid")
-		$("#due").val(due);
-
+		$("#sub_total").unmask().val(sub_total).mask("#.##0,00", {reverse: true});
+		$("#total").unmask().val($("#sub_total").cleanVal()-$("#discount").cleanVal()).mask("#.##0,00", {reverse: true});
 	};
 
 	$("#discount").keyup(function(){
@@ -99,39 +76,25 @@ $(document).ready(function(){
 		calculate(discount,0);
 	});
 
-	$("#paid").keyup(function(){
-		var paid = $(this).val();
-		var discount = $("#discount").val();
-		calculate(discount,paid);
-	});
-
-
 	/*Order Accepting*/
 
 	$("#order_form").click(function(){
-		var invoice = $("#get_order_data").serialize();
-		if ($("#cust_name").val() === "") {
-			alert("Plaese enter customer name");
+		if($('.itemrow').length < 1){
+			alert("Adicione algum item ao pedido!");
 		}else if($("#paid").val() === ""){
-			alert("Plaese eneter paid amount");
+			alert("Entre com o valor pago!");
 		}else{
-			$.ajax({
-				url : DOMAIN+"/includes/process.php",
-				method : "POST",
-				data : $("#get_order_data").serialize(),
-				success : function(data){
+			funfo = 0;
+			if (funfo < 0) {
+				alert(data);
+			}else{
+				var invoice = $("#get_order_data").serialize();
+				$("#get_order_data").trigger("reset");
 
-					if (data < 0) {
-						alert(data);
-					}else{
-						$("#get_order_data").trigger("reset");
-
-						if (confirm("Do u want to print invoice ?")) {
-							window.location.href = DOMAIN+"/includes/invoice_bill.php?invoice_no="+data+"&"+invoice;
-						}
-					}
+				if (confirm("Gostaria de imprimir o cupom não fiscal?")) {
+					
 				}
-			});
+			}
 		}
 	});
 
