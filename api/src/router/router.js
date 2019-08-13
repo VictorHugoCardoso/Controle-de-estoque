994a00c439342118450feae70cde65ba4163e7b9
PaulloWeber <paulloweber@gmail.com>
2019-08-12 23:51:45 -0300

    retorno do produto

--- api/src/router/router.js
+++ api/src/router/router.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import usuarioRoutes from '../api/usuario/usuario.routes'
+import produtoRoutes from '../api/produto/produto.routes'
 
 
 const router = express.Router()
 
 
 router.use('/usuario', usuarioRoutes)
+router.use('/produto', produtoRoutes)
 
 
 export default router
