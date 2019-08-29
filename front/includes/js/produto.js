e1d248e7a9e40d0b9e7fb403d896d1fcd97096f0
note <victor.mendes056@gmail.com>
2019-08-28 22:45:10 -0300

    xml funcionando

--- front/includes/js/produto.js
+++ front/includes/js/produto.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
     shortcut.add("F1", function() {
 		$("#order_form").click();
-	});
+    });
+
+    $('#arquivoXML').on('change', function(){
+        var file = $('#arquivoXML').prop('files')[0];
+        console.log(file);
+        $("#labelFile").html(file['name']);
+
+        if (file) {
+            var reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+            reader.onload = function(e) {
+                var content = e.target.result;
+                var xml = $.parseXML(content);
+                var $produtos = $(xml).find('prod');
+                console.log($produtos);
+                $produtos.each(function(){
+                    var nomeProduto = $(this).find('xProd').text();
+                    console.log(nomeProduto);
+                });
+             };
+        }else{
+            alert('Entre com algum arquivo!');
+        }
+    });
     
     $("#sendForm").click(function(e){
 		e.preventDefault();
